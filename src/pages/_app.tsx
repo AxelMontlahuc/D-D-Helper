908c104553b9d78e25fd51ebc8bdfc882587864d
AxelMontlahuc <axel.montlahuc@proton.me>
2025-06-23 22:53:39 +0200

    First navbar prototype

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,54 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import  { ThemeProvider } from "@/components/theme-provider";
+import { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuLink } from "@/components/ui/navigation-menu";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
+            <div className="flex justify-between m-[20px]">
+              <NavigationMenu>
+                    <NavigationMenuList>
+                      <NavigationMenuItem>
+                        <NavigationMenuLink asChild>
+                          <Link href="/">Home</Link>
+                        </NavigationMenuLink>
+                      </NavigationMenuItem>
+                      <NavigationMenuItem>
+                        <NavigationMenuLink asChild>
+                          <Link href="/">Characters</Link>
+                        </NavigationMenuLink>
+                      </NavigationMenuItem>
+                      <NavigationMenuItem>
+                        <NavigationMenuLink asChild>
+                          <Link href="/">Campaigns</Link>
+                        </NavigationMenuLink>
+                      </NavigationMenuItem>
+                    </NavigationMenuList>
+                  </NavigationMenu>
+
+                  <NavigationMenu>
+                    <NavigationMenuList>
+                      <NavigationMenuItem>
+                        <NavigationMenuLink asChild>
+                          <Link href="/">Account</Link>
+                        </NavigationMenuLink>
+                      </NavigationMenuItem>
+                      <NavigationMenuItem>
+                        <NavigationMenuLink asChild>
+                          <Link href="/">Settings</Link>
+                        </NavigationMenuLink>
+                      </NavigationMenuItem>
+                    </NavigationMenuList>
+                  </NavigationMenu>
+            </div>
+
             <Component {...pageProps} />
           </ThemeProvider>
   );
